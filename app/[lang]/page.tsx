78db019bd3dc9f7a2925d13de9630b164d89e4ed
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2025-10-04 14:21:57 +0200

    v1.1.0a

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -18,7 +18,7 @@ async function getMusicFiles() {
 
 export default async function Home({ params }: { params: { lang: string } }) {
   const musicFiles = await getMusicFiles();
-  const lang = ["es", "en"].includes(params.lang) ? params.lang : "en";
+  const lang = (["es", "en"].includes(params.lang) ? params.lang : "en") as "es" | "en";
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
